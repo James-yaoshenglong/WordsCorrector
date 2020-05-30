2825eae5246a4daf7e0514ec0f046d715db5aab6
James-yaoshenglong <yaoshenglong758063348@gmail.com>
2020-05-30 17:26:02 +0800

    solve bug in command transfer

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,38 @@ int main(int argc, char* argv[]){
         giveHelp();
         return 1;
     }
-    else if(argc < 3){
-        cout<<"Need more arguments!"<<endl;
-        giveHelp();
-        return 1;
-    }
-    else if(strcmp(argv[1], "--help") == 0){
+    else if(argc == 1){
+        cout<<"Welcome to words corrector!"<<endl;
         giveHelp();
         return 0;
     }
-    else if(strcmp(argv[1], "-s") == 0){// check single word
-        char* word = argv[2];
-        SingalCorrector p(word);
-        p.start();
-        return 0;
+    else if(argc == 2){
+        if(strcmp(argv[1], "--help") == 0){
+            giveHelp();
+            return 0;
+        }
+        else{
+            cout<<"Need more arguments!"<<endl;
+        }
     }
-    else if(strcmp(argv[1], "-f") == 0){// check a file
-        char* filename = argv[2];
-        FileCorrector p(filename);
-        p.start();
-        return 0;
-    }
-    else{
-        cout<<"Argument not finded"<<endl;
-        giveHelp();
-        return 1;
+    else if(argc == 3){
+         if(strcmp(argv[1], "-s") == 0){// check single word
+            char* word = argv[2];
+            SingalCorrector p(word);
+            p.start();
+            return 0;
+        }
+        else if(strcmp(argv[1], "-f") == 0){// check a file
+            char* filename = argv[2];
+            FileCorrector p(filename);
+            p.start();
+            return 0;
+        }
+        else{
+            cout<<"Argument not finded"<<endl;
+            giveHelp();
+            return 1;
+        }
     }
     return 0;
 }
